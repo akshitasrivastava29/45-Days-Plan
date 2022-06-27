f3d10b7f02611abab04ee3033a35aec5808ac530
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-06-27 16:14:23 +0530

    Time: 290 ms (58.82%), Space: 127 MB (65.00%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,47 +2,55 @@ class Solution {
     
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-//         int n=nums.size();
-//         int st=0,end=n-1;
-//          for(int i=0;i<n;i++)
-//          {
-//              if(nums[st]<pivot)
-//              {
-//                  st++;
-//              }
-//              else if(nums[end]>pivot)
-//              {
-//                  end--;
-//              }
-//              else if(nums[st] >pivot && nums[end] <pivot)
-//              {
-//                  swap(nums[st],nums[end]);
-//              }
-            
-//          }
-//         return nums;
+//         
+        // vector<int>ans;
+        // for(auto it:nums)
+        // {
+        //     if(it<pivot)
+        //     {
+        //         ans.push_back(it);
+        //     }
+        // }
+        // for(auto it:nums)
+        // {
+        //     if(it==pivot)
+        //     {
+        //         ans.push_back(it);
+        //     }
+        // }
+        // for(auto it:nums)
+        // {
+        //     if(it>pivot)
+        //     {
+        //         ans.push_back(it);
+        //     }
+        // }
+        // return ans;
         vector<int>ans;
-        for(auto it:nums)
+        int j=0,cnt=0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(it<pivot)
+            if(nums[i]<pivot)
             {
-                ans.push_back(it);
+                nums[j++]=nums[i];
+            }
+            else if(nums[i]==pivot)
+            {
+                cnt++;
+            }
+            else
+            {
+                ans.push_back(nums[i]);
             }
         }
-        for(auto it:nums)
+        while(cnt--)
         {
-            if(it==pivot)
-            {
-                ans.push_back(it);
-            }
+            nums[j++]=pivot;
         }
-        for(auto it:nums)
+        for(int i=0;i<ans.size();i++)
         {
-            if(it>pivot)
-            {
-                ans.push_back(it);
-            }
+            nums[j++]=ans[i];
         }
-        return ans;
+        return nums;
     }
 };
