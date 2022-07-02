aa9f864df4be417db240941f40f4f66ee7a00623
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-02 15:08:51 +0530

    Time: 3 ms (51.06%), Space: 6.5 MB (7.43%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -34,8 +34,50 @@ public:
 //             st2.pop();
 //         }
 //         return v1==v2;
-        stack<int>st1;
-        stack<int>st2;
+        
+//         stack<int>st1;
+//         stack<int>st2;
+//         string v1,v2;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             if(s[i]!='#')
+//             {
+//                 st1.push(s[i]);
+//             }
+//              else if(s[i]=='#' && !st1.empty())
+//                 {
+//                     st1.pop();
+//                 }
+//         }
+//         while(!st1.empty())
+//         {
+//             v1.push_back(st1.top());
+//             st1.pop();
+//         }
+//         for(int i=0;i<t.length();i++)
+//         {
+//             if(t[i]!='#')
+//             {
+//                 st2.push(t[i]);
+//             }
+//             else if(t[i]=='#' && !st2.empty())
+//             {
+//                 st2.pop();
+//             }
+//         }
+//         while(!st2.empty())
+//         {
+//             v2.push_back(st2.top());
+//                 st2.pop();
+//         }
+        
+//         if(v1==v2)
+//         {
+//             return true;
+//         }
+//         return false;
+        stack<char>st1;
+        stack<char>st2;
         string v1,v2;
         for(int i=0;i<s.length();i++)
         {
@@ -43,10 +85,10 @@ public:
             {
                 st1.push(s[i]);
             }
-             else if(s[i]=='#' && !st1.empty())
-                {
-                    st1.pop();
-                }
+            else if(s[i]=='#' && !st1.empty())
+            {
+                st1.pop();
+            }
         }
         while(!st1.empty())
         {
@@ -67,9 +109,8 @@ public:
         while(!st2.empty())
         {
             v2.push_back(st2.top());
-                st2.pop();
+            st2.pop();
         }
-        
         if(v1==v2)
         {
             return true;
