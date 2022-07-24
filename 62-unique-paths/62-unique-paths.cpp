db642a0b0b3e42cf43f102f324d51fc2fe7b49ab
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-24 16:30:40 +0530

    Time: 5 ms (18.38%), Space: 5.9 MB (73.52%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -147,9 +147,31 @@ public:
         // return dp[m-1][n-1];
         // ---------
         
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+//         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         
-        return getcnt(m-1,n-1,dp);
+//         return getcnt(m-1,n-1,dp);
+        // ________________-----TAbULATON
+        
+        int dp[m][n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0) up+=dp[i-1][j];
+                    if(j>0) left+=dp[i][j-1];
+                    dp[i][j]=up+left;
+                    
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
