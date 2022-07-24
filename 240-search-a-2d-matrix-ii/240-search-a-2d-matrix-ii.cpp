399332389430f642c39e929e0d647c0ee694b982
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-24 15:54:40 +0530

    Time: 204 ms (37.62%), Space: 14.8 MB (88.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -42,6 +42,24 @@ public:
         //     }
         // return false; 
         
+        // int i=0,j=m-1;
+        // while(i<n && j>=0)
+        // {
+        //     if(matrix[i][j]==target)
+        //     {
+        //         return true;
+        //     }
+        //     else if(matrix[i][j]>target)
+        //     {
+        //         j--;
+        //     }
+        //     else
+        //     {
+        //         i++;
+        //     }
+        // }
+        // return false;
+        
         int i=0,j=m-1;
         while(i<n && j>=0)
         {
