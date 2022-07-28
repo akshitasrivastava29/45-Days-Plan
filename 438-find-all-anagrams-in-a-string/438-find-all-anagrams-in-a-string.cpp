3740e3518c99a7c9128cb6b2ac8ac3f3d557b327
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-28 23:43:38 +0530

    Time: 32 ms (37.75%), Space: 8.8 MB (38.88%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -37,12 +37,52 @@ public:
         //     }
         // }
         // return v;
+        // -----------------------
+        // unordered_map<char,int>mp;
+        // vector<int>ans;
+        // for(int i=0;i<p.length();i++)
+        // {
+        //     mp[p[i]]++;
+        //  }
+        // int cnt=mp.size();
+        // int i=0,j=0,k=p.size();
+        // while(j<s.length())
+        // {
+        //     if(mp.find(s[j])!=mp.end())
+        //     {
+        //         mp[s[j]]--;
+        //         if(mp[s[j]]==0)
+        //             cnt--;
+        //     }
+        //     if(j-i+1<k)
+        //     {
+        //         j++;
+        //     }
+        //     else if(j-i+1==k)
+        //     {
+        //         if(cnt==0)
+        //         {
+        //             ans.push_back(i);
+        //         }
+        //         if(mp.find(s[i])!=mp.end())
+        //         {
+        //             mp[s[i]]++;
+        //             if(mp[s[i]]==1)
+        //             {
+        //                 cnt++;
+        //             }
+        //         }
+        //         i++,j++;
+        //     }
+        // }
+        // return ans;
+        // -------------
         unordered_map<char,int>mp;
         vector<int>ans;
         for(int i=0;i<p.length();i++)
         {
             mp[p[i]]++;
-         }
+        }
         int cnt=mp.size();
         int i=0,j=0,k=p.size();
         while(j<s.length())
@@ -51,9 +91,11 @@ public:
             {
                 mp[s[j]]--;
                 if(mp[s[j]]==0)
+                {
                     cnt--;
+                }
             }
-            if(j-i+1<k)
+           if(j-i+1<k)
             {
                 j++;
             }
