308103ec8389bfd746e46398f30e3a6caa71e097
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-07-30 01:23:55 +0530

    Time: 90 ms (94.24%), Space: 41.4 MB (88.47%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -44,6 +44,23 @@ public:
 //     return cnt;
         
         
+        // int cnt=0,sum=0;
+        // unordered_map<int ,int>mp;
+        // for(int i=0;i<n;i++)
+        // {
+        //     sum+=nums[i];
+        //     if(sum==k)
+        //     {
+        //         cnt++;
+        //     }
+        //     if(mp.find(sum-k)!=mp.end())
+        //     {
+        //         cnt+=mp[sum-k];
+        //     }
+        //     mp[sum]++;
+        // }
+        // return cnt;
+        // -----------------
         int cnt=0,sum=0;
         unordered_map<int ,int>mp;
         for(int i=0;i<n;i++)
