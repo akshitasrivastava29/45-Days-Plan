a3e8413c0fccc519b51b27a11974050f014d4180
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-02 00:32:52 +0530

    Time: 478 ms (23.42%), Space: 10.2 MB (70.35%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,65 @@
+class Solution {
+    // private:
+    // long long sieve(int n)
+    // {
+    //     vector<bool>primes(n+1,true);
+    //     int cnt=0;
+    //     for(long long i=2;i<n;i++)
+    //     {
+    //         if(primes[i]==true)
+    //         {
+    //             cnt++;
+    //         }
+    //         for(long long j=i*i;j<=n;j+=i)
+    //         {
+    //             primes[j]=false;
+    //         }
+    //     }
+    //     return cnt;
+    // }
+    
+    // private:
+    //  long long sieve(int n)
+    //  {
+    //     vector<bool>primes(n+1,true);
+    //      int cnt=0;
+    //      for(long long i=2;i<n;i++)
+    //      {
+    //        if(primes[i]==true)
+    //        {
+    //             cnt++;
+    //        }
+    //          for(long long j=i*i;j<=n;j+=i)
+    //          {
+    //             primes[j]=false;
+    //          }
+    //      }
+    //      return cnt;
+     // }
+    private:
+    long long sieve(int n)
+    {
+        vector<bool>primes(n+1,true);
+        int cnt=0;
+        for(long long i=2;i<n;i++)
+        {
+            if(primes[i]==true)
+            {
+                cnt++;
+            }
+            for(long long j=i*i;j<=n;j+=i)
+            {
+                primes[j]=false;
+            }
+        }
+        return cnt;
+    }
+public:
+    int countPrimes(int n) {
+         return sieve(n);
+        
+        // ---------------------
+        
+        
+    }
+};
