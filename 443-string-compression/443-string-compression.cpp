7784c86491f3c232618a657e9b057ebc5bb4f49e
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-01 23:43:29 +0530

    Time: 8 ms (55.33%), Space: 8.9 MB (94.00%) - LeetHub

--- 443-string-compression/443-string-compression.cpp
+++ 443-string-compression/443-string-compression.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i=0;
+//         int i=0;
+//         int n=chars.size();
+//         int k=0;
+//         while(i<n)
+//         {
+//             int j=i;
+//             while(j<n && chars[i]==chars[j])
+//             {
+//                 j++;
+//             }
+//             chars[k++]=chars[i];
+//             if(j-i>1)
+//             {
+//                 for(char c:to_string(j-i))
+//                 {
+//                     chars[k++]=c;
+//                 }
+//             }
+            
+//             i=j;
+        
+//         }
+        
+//         return k;
+         
         int n=chars.size();
-        int k=0;
+        int i=0;
+        int ansidx=0;
         while(i<n)
         {
-            int j=i;
+            int j=i+1;
             while(j<n && chars[i]==chars[j])
             {
                 j++;
             }
-            chars[k++]=chars[i];
-            if(j-i>1)
+            chars[ansidx++]=chars[i];
+            int count=j-i;
+            if(count>1)
             {
-                for(char c:to_string(j-i))
+                string cnt=to_string(count);
+                for(char ch:cnt)
                 {
-                    chars[k++]=c;
+                    chars[ansidx++]=ch;
                 }
             }
-            
             i=j;
-        
         }
-        
-        return k;
-         
-        
+        return ansidx;
         
     }
 };
