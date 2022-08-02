30279009e07daca1c4942aea9ef82f34ab1ba466
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-02 12:06:45 +0530

    Time: 3 ms (74.27%), Space: 7 MB (96.01%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -14,13 +14,28 @@ class Solution {
 //         fun(ind+1,nums,temp,v);
         
 //     }
+    // -------------
+//     void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
+//     {
+//         int n=nums.size();
+//         if(ind==n)
+//         {
+//             ans.push_back(temp);
+//             return ;
+//         }
+//         temp.push_back(nums[ind]);
+//         getsubsets(ind+1,nums,temp,ans);
+//         temp.pop_back();
+//         getsubsets(ind+1,nums,temp,ans);
+        
+//     }
+    // -------
     void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
     {
-        int n=nums.size();
-        if(ind==n)
+        if(ind==nums.size())
         {
             ans.push_back(temp);
-            return ;
+            return;
         }
         temp.push_back(nums[ind]);
         getsubsets(ind+1,nums,temp,ans);
