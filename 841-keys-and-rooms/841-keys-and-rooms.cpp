d3a69910d6ce8206d4c095db51045db53fcb6b1c
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-04 00:32:55 +0530

    Time: 38 ms (5.47%), Space: 10.4 MB (79.17%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,28 +1,59 @@
 class Solution {
-    void dfs(int node,vector<vector<int>>&rooms,vector<int>&vis)
-    {
-        vis[node]=1;
-        for(auto it:rooms[node])
-        {
-            if(vis[it]==-1)
-            {
-                dfs(it,rooms,vis);
-            }
-        }
-    }
+    // void dfs(int node,vector<vector<int>>&rooms,vector<int>&vis)
+    // {
+    //     vis[node]=1;
+    //     for(auto it:rooms[node])
+    //     {
+    //         if(vis[it]==-1)
+    //         {
+    //             dfs(it,rooms,vis);
+    //         }
+    //     }
+    // }
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        // int n=rooms.size();
+        // vector<int>vis(n,-1);
+        // dfs(0,rooms,vis);
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(vis[i]==-1)
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        
         int n=rooms.size();
-        vector<int>vis(n,-1);
-        dfs(0,rooms,vis);
-        for(int i=0;i<n;i++)
+        vector<int>vis(n,0);
+        queue<int>q;
+        vis[0]=1;
+        q.push(0);
+        while(q.size())
         {
-            if(vis[i]==-1)
+            int node=q.front();
+            q.pop();
+            for(auto it:rooms[node])
             {
-                return false;
+                if(!vis[it])
+                {
+                    vis[it]=1;
+                    q.push(it);
+                }
             }
         }
-        return true;
+        for(int i=0;i<n;i++)
+        {   
+        
+            if(!vis[i])
+            
+                return false;
+        }
+            
+        
+         return true;
+      
         
     }
 };
