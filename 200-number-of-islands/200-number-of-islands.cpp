6eeb197effa8290f0611926470dde14755e7f5c5
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-03 23:15:45 +0530

    Time: 39 ms (88.53%), Space: 12.6 MB (34.49%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,18 +1,32 @@
 class Solution {
     private:
-    void dfs(vector<vector<char>>&grid,vector<vector<bool>>&vis ,int i, int j)
-    {
-        // int m=grid.size();
-        // int n=grid[0].size();
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() ||grid[i][j]=='0'|| vis[i][j]==true)
-            return ;
+//     void dfs(vector<vector<char>>&grid,vector<vector<bool>>&vis ,int i, int j)
+     // {
+//         // int m=grid.size();
+//         // int n=grid[0].size();
+//         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() ||grid[i][j]=='0'|| vis[i][j]==true)
+//             return ;
     
+//         vis[i][j]=true;
+//         dfs(grid, vis, i - 1, j); // TOP
+//         dfs(grid, vis, i + 1, j); // DOWN
+//         dfs(grid, vis, i, j - 1); // LEFT
+//        dfs(grid, vis, i, j + 1); // RIGHT
+//     }
+    void dfs(vector<vector<char>>&grid,vector<vector<bool>>&vis,int i,int j)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0' || vis[i][j]==true)return ;
         vis[i][j]=true;
-        dfs(grid, vis, i - 1, j); // TOP
-        dfs(grid, vis, i + 1, j); // DOWN
-        dfs(grid, vis, i, j - 1); // LEFT
-       dfs(grid, vis, i, j + 1); // RIGHT
+        dfs(grid,vis,i-1,j);
+        dfs(grid,vis,i,j+1);
+        dfs(grid,vis,i+1,j);
+        dfs(grid,vis,i,j-1);
+        
     }
+
+            
 public:
     int numIslands(vector<vector<char>>& grid) {
 //         int m=grid.size();
@@ -33,6 +47,24 @@ public:
         
 //         return cnt;
         
+        // int m=grid.size();
+        // int n=grid[0].size();
+        // int cnt=0;
+        // vector<vector<bool>>vis(m,vector<bool>(n,false));
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(grid[i][j]=='1' && vis[i][j]==false)
+        //         {
+        //             dfs(grid,vis,i,j);
+        //             cnt+=1;
+        //         }
+        //     }
+        // }
+        // return cnt;
+        
+        
         int m=grid.size();
         int n=grid[0].size();
         int cnt=0;
@@ -47,6 +79,7 @@ public:
                     cnt+=1;
                 }
             }
+            
         }
         return cnt;
 //     public:
