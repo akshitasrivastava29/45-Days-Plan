c45101be2ed8020eace5f846908497b7f7daa864
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-06 22:23:24 +0530

    Time: 230 ms (15.26%), Space: 43.9 MB (19.09%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -44,19 +44,49 @@ public:
             
 //         }
         // return cycles;
-         unordered_map<char,int> mp;
+        // ---------------
+//          unordered_map<char,int> mp;
+//         for(auto x:tasks)
+//             mp[x]++;
+        
+//         priority_queue<int> pq;
+//         for(auto x:mp)
+//             pq.push(x.second);
+        
+//         int ans=0;
+//         while(!pq.empty())
+//         {
+//             vector<int> t;
+//             for(int i=0;i<=n && !pq.empty();i++)
+//             {
+//                 t.push_back(pq.top());
+//                 pq.pop();
+//             }
+//             for(auto x:t)
+//             {
+//                 if(x-1>0)
+//                     pq.push(x-1);
+//             }
+            
+//             ans += pq.empty()? t.size(): n+1;
+//         }
+//         return ans;
+        // -------------
+        unordered_map<char,int>mp;
         for(auto x:tasks)
+        {
             mp[x]++;
-        
-        priority_queue<int> pq;
+        }
+        priority_queue<int>pq;
         for(auto x:mp)
+        {
             pq.push(x.second);
-        
+        }
         int ans=0;
         while(!pq.empty())
         {
-            vector<int> t;
-            for(int i=0;i<=n && !pq.empty();i++)
+            vector<int>t;
+            for(int i=0;i<=n && !pq.empty() ;i++)
             {
                 t.push_back(pq.top());
                 pq.pop();
@@ -64,10 +94,11 @@ public:
             for(auto x:t)
             {
                 if(x-1>0)
+                {
                     pq.push(x-1);
+                }
             }
-            
-            ans += pq.empty()? t.size(): n+1;
+            ans+=pq.empty()? t.size(): n+1;
         }
         return ans;
     }
