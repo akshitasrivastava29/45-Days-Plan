933ba1fa3e68333f8301592d3f5b336bf0d903ed
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-07 01:29:52 +0530

    Time: 30 ms (40.37%), Space: 17 MB (74.81%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+//         vector<vector<int>>res;
+//         int n=intervals.size();
+        
+//         // sort(intervals.begin(),intervals.end());
+//         for(int i=0;i<n;i++)
+//         {
+//             if(intervals[i][1] <newInterval[0])
+//             {
+//                 res.push_back(intervals[i]);
+//             }
+//             else if(intervals[i][0]>newInterval[1])
+//             {
+//                 res.push_back(newInterval);
+//                 newInterval=intervals[i];
+//             }
+//             else if(intervals[i][1]>=newInterval[0] || intervals[i][0] <=newInterval[1])
+//             {
+//                 newInterval[0]=min(intervals[i][0],newInterval[0]);
+//                 newInterval[1]=max(intervals[i][1],newInterval[1]);
+//             }
+            
+//         }
+//         res.push_back(newInterval);
+//          return res;
+        vector<vector<int>>res;
+        int n=intervals.size();
+        for(int i=0;i<n;i++)
+        {
+            if(intervals[i][1]<newInterval[0])
+            {
+                res.push_back(intervals[i]);
+            }
+            else if(intervals[i][0]>newInterval[1])
+            {
+                res.push_back(newInterval);
+                newInterval=intervals[i];
+            }
+            else if(intervals[i][1]>=newInterval[0] || intervals[i][0]<newInterval[1])
+            {
+                newInterval[0]=min(intervals[i][0],newInterval[0]);
+                newInterval[1]=max(intervals[i][1],newInterval[1]);
+            }
+        }
+        res.push_back(newInterval);
+        return res;
+        
+    }
+};
