9d143fe2cb84f0446db202ca891e73e6869b37d9
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-08 01:46:18 +0530

    Time: 183 ms (16.26%), Space: 34.1 MB (21.78%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -40,24 +40,42 @@ public:
         // sort(ans.begin(),ans.end());
         // return ans;
         
+        // int n=arr.size();
+        // vector<int>ans;
+        // priority_queue<pair<int,int>>pq;
+        // for(int i=0;i<n;i++)
+        // {
+        //     pq.push({abs(x-arr[i]),arr[i]});
+        //     while(pq.size()>k)
+        //     {
+        //         pq.pop();
+        //     }
+        // }
+        // while(pq.size()>0)
+        // {
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
+        // -----------
         int n=arr.size();
-        vector<int>ans;
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>>maxh;
         for(int i=0;i<n;i++)
         {
-            pq.push({abs(x-arr[i]),arr[i]});
-            while(pq.size()>k)
+            maxh.push({abs(x-arr[i]),arr[i]});
+            while(maxh.size()>k)
             {
-                pq.pop();
+                maxh.pop();
             }
         }
-        while(pq.size()>0)
+        vector<int>ans;
+        while(maxh.size()>0)
         {
-            ans.push_back(pq.top().second);
-            pq.pop();
+            ans.push_back(maxh.top().second);
+            maxh.pop();
         }
         sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
