219728768e2ff39b9a2f12945c5b7fd2b9a16e6d
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-09 00:23:21 +0530

    Time: 8 ms (97.95%), Space: 10.7 MB (86.68%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,118 +1,8 @@
 class Solution {
-    
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-//      int n=str1.length();
-//     int m=str2.length();
-//         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-//         for(int i=0;i<=n;i++)
-//             dp[i][0]=0;
-//         for(int j=0;j<=m;j++)
-//             dp[0][j]=0;
-//         for(int i=1;i<=n;i++)
-//         {
-//             for(int j=1;j<=m;j++)
-//             {
-//              if(str1[i-1]==str2[j-1])
-//              {
-//                  dp[i][j]=1+dp[i-1][j-1];
-//              }
-//             else
-//             {
-//                 dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-//             }
-//         }
-
-//         }
-//         string ans="";
-//         int  i=n,j=m;
-//         while(i>0&& j>0)
-//         {
-//             if(str1[i-1]==str2[j-1])
-//             {
-//                 ans.push_back(str1[i-1]);
-//                 i--,j--;
-//             }
-//            else  if(dp[i-1][j]>dp[i][j-1])
-//             {
-//                 ans.push_back(str1[i-1]);   
-//                i--;
-//             }
-//              else
-//              {
-//                 ans.push_back(str2[j-1]);
-//                  j--;
-//              }
-//          }
-//         while(i>0){
-//         ans.push_back(str1[i-1]);
-//         i--;
-//          }
-//         while(j>0){
-//         ans.push_back(str2[j-1]);
-//          j--;
-//         }
-                            
-//        reverse(ans.begin(),ans.end());
-//         return ans;
-        // ---------------------
-//         int n=str1.length();
-//         int m=str2.length();
-//         int dp[n+1][m+1];
-//         for(int i=0;i<=n;i++)
-//             dp[i][0]=0;
-//         for(int j=0;j<=m;j++)
-//             dp[0][j]=0;
-//         for(int i=1;i<=n;i++)
-//         {
-//             for(int j=1;j<=m;j++)
-//             {
-//                 if(str1[i-1]==str2[j-1])
-//                 {
-//                     dp[i][j]=1+dp[i-1][j-1];
-//                 }
-//                 else 
-//                 {
-//                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-//                 }
-//             }
-//         }
-//         string ans="";
-//         int i=n,j=m;
-//         while(i>0 && j>0)
-//         {
-//             if(str1[i-1]==str2[j-1])
-//             {
-//                 ans.push_back(str1[i-1]);
-//                 i--,j--;
-//             }
-//             else if(dp[i-1][j]>dp[i][j-1])
-//             {
-//                 ans.push_back(str1[i-1]);
-//                 i--;
-//             }
-//             else
-//             {
-//                 ans.push_back(str2[j-1]);
-//                 j--;
-//             }
-//         }
-//         while(i>0)
-//         {
-//             ans.push_back(str1[i-1]);
-        
-//         i--;
-//         }
-//         while(j>0)
-//         {
-//             ans.push_back(str2[j-1]);
-//         j--;
-//         }
-//         reverse(ans.begin(),ans.end());
-//         return ans;
-        
-        int n=str1.length();
-        int m=str2.length();
+        int n=str1.size();
+        int m=str2.size();
         int dp[n+1][m+1];
         for(int i=0;i<=n;i++)dp[i][0]=0;
         for(int j=0;j<=m;j++)dp[0][j]=0;
@@ -120,9 +10,9 @@ public:
         {
             for(int j=1;j<=m;j++)
             {
-                if(str1[i-1]==str2[j-1])
+               if(str1[i-1]==str2[j-1])
                 {
-                    dp[i][j]=1+dp[i-1][j-1];
+                   dp[i][j]=1+dp[i-1][j-1] ;
                 }
                 else
                 {
@@ -136,32 +26,34 @@ public:
         {
             if(str1[i-1]==str2[j-1])
             {
-                ans.push_back(str1[i-1]);
+                ans+=str1[i-1];
                 i--,j--;
             }
-            else if(dp[i-1][j]>dp[i][j-1])
-            {
-                ans.push_back(str1[i-1]);
-                i--;
-            }
+              else if(dp[i-1][j]>dp[i][j-1])
+              {
+                  ans+=str1[i-1];
+                  i--;
+              }
             else
             {
-                ans.push_back(str2[j-1]);
+                ans+=str2[j-1];
                 j--;
             }
         }
         while(i>0)
         {
-            ans.push_back(str1[i-1]);
-            i--;
+                ans+=str1[i-1];
+                i--;
+            
         }
         while(j>0)
         {
-            ans.push_back(str2[j-1]);
+            ans+=str2[j-1];
             j--;
         }
         reverse(ans.begin(),ans.end());
         return ans;
-            
+        
+        
     }
 };
