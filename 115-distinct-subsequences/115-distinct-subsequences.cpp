a3dcbff5912b482b8c01b41b157a40606848b432
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-09 14:58:10 +0530

    Time: 28 ms (91.70%), Space: 15 MB (72.24%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,7 +1,8 @@
 class Solution {
-    // private:
-    // int num(string s,string t,int i,int j,vector<vector<int>>&dp)
+    // int fun(int i,int j,string  s,string t,vector<vector<int>>&dp)
     // {
+    //     int n=s.length();
+    //     int m=t.length();
     //     if(j<0)return 1;
     //     if(i<0)return 0;
     //     if(dp[i][j]!=-1)
@@ -10,73 +11,31 @@ class Solution {
     //     }
     //     if(s[i]==t[j])
     //     {
-    //         return dp[i][j]= num(s,t,i-1,j-1,dp)+num(s,t,i-1,j,dp);
+    //       return dp[i][j]=  fun(i-1,j-1,s,t,dp)+fun(i-1,j,s,t,dp);
     //     }
-    //     return dp[i][j]= num(s,t,i-1,j,dp);
+    //     else
+    //     {
+    //        return dp[i][j]= fun(i-1,j,s,t,dp);
+    //     }
     // }
-//     private:
-//     int num(string&s,string &t,int i,int j,vector<vector<int>>&dp)
-//     {
-        
-//         if(j<0)return 1;
-//         if(i<0)return 0;
-//         if(dp[i][j]!=-1)
-//         {
-//             return dp[i][j];
-//         }
-//         if(s[i]==t[j])
-//         {
-//             return  dp[i][j]= num(s,t,i-1,j-1,dp)+num(s,t,i-1,j,dp);
-//         }
-//         else
-//         {
-//             return  dp[i][j]= num(s,t,i-1,j,dp);
-//         }
-//     }
-   
 public:
     int numDistinct(string s, string t) {
-//         int n=s.size();
-//         int m=t.size();
-//         vector<vector<double>>dp(n+1,vector<double>(m+1,0));
-//         for(int i=0;i<=n;i++)
-//             dp[i][0]=1;
-//         for(int j=1;j<=m;j++)
-//             dp[0][j]=0;
-//         for(int i=1;i<=n;i++)
-//         {
-//             for(int j=1;j<=m;j++)
-//             {
-//                 if(s[i-1]==t[j-1])
-//                 {
-//                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-//                 }
-//                 else
-//                 {
-//                     dp[i][j]=dp[i-1][j];
-//                 }
-//             }
-//         }
-         
-//        return (int) dp[n][m]; 
-        // --------
-        // int n=s.length();
-        // int m=t.length();
-        // vector<vector<int>>dp(n,vector<int>(m,-1));
-        // return num(s,t,n-1,m-1,dp);
-        // ----------
-        int n=s.length();
+         int n=s.length();
         int m=t.length();
-        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=1;
-        for(int j=1;j<=m;j++)dp[0][j]=0;
-        for(int i=1;i<=n;i++)
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return fun(n-1,m-1,s,t,dp);
+        
+        // TABULATION
+         double dp[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for( int j=1;j<=m;j++) dp[0][j]=0;
+        for( int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
                 if(s[i-1]==t[j-1])
                 {
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
                 }
                 else
                 {
@@ -84,8 +43,6 @@ public:
                 }
             }
         }
-        return  (int)dp[n][m];
-        
-        
+        return (int)dp[n][m];
     }
 };
