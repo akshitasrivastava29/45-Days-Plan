7ce8796e3de163d64394d4d06b74becc2d878304
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-10 00:59:37 +0530

    Time: 125 ms (69.02%), Space: 41.3 MB (95.22%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,69 +1,10 @@
 class Solution {
-    
-    
-   
-    
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-//         int cnt=0;
-//         int sum=0;
-//         unordered_map<int,int>mp;
-//         for(int i=0;i<n;i++)
-//         {
-//             sum+=nums[i];
-//             if(sum==k)
-//             {
-//                 cnt++;
-//             }
-//             if(mp.find(sum-k)!=mp.end())
-//             {
-//                 cnt+=mp[sum-k];
-//             }
-//             mp[sum]++;
-            
-//         }
-//       return cnt;  
-        
-//         int sum=0,cnt=0;
-//         unordered_map<int,int>mp;
-//         for(int i=0;i<n;i++)
-//         {
-//            sum+=nums[i];
-//             if(sum==k)
-//             {
-//                 cnt++;
-//             }
-//             if(mp.find(sum-k)!=mp.end())
-
-//             {
-//                 cnt+=mp[sum-k];
-//             }
-//             mp[sum]++;
-//         }
-//     return cnt;
-        
-        
-        // int cnt=0,sum=0;
-        // unordered_map<int ,int>mp;
-        // for(int i=0;i<n;i++)
-        // {
-        //     sum+=nums[i];
-        //     if(sum==k)
-        //     {
-        //         cnt++;
-        //     }
-        //     if(mp.find(sum-k)!=mp.end())
-        //     {
-        //         cnt+=mp[sum-k];
-        //     }
-        //     mp[sum]++;
-        // }
-        // return cnt;
-        // -----------------
-        int cnt=0,sum=0;
-        unordered_map<int ,int>mp;
-        for(int i=0;i<n;i++)
+        unordered_map<int,int>mp;
+        int sum=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
             if(sum==k)
@@ -76,6 +17,7 @@ public:
             }
             mp[sum]++;
         }
-        return cnt;
+            return cnt;
+        
     }
 };
