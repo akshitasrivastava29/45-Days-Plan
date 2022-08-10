df78eb5599b9fd19b3607749a17690a2c18fd740
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-11 00:42:58 +0530

    Time: 23 ms (60.11%), Space: 8.8 MB (22.62%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,91 +1,17 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        // unordered_map<char,int>mp;
-        // vector<int>v;
-        // for(int i=0;i<p.size();i++)
-        // {
-        //     mp[p[i]]++;
-        // }
-        // int cnt=mp.size(),i=0,j=0,k=p.size();
-        // while(j<s.length())
-        // {
-        //     if(mp.find(s[j])!=mp.end())
-        //     {
-        //         mp[s[j]]--;
-        //         if(mp[s[j]]==0)
-        //             cnt--;
-        //     }
-        //     if(j-i+1<k)
-        //     {
-        //         j++;
-        //     }
-        //     else if(j-i+1==k)
-        //     {
-        //         if(cnt==0)
-        //         {
-        //             v.push_back(i);
-        //         }
-        //         if(mp.find(s[i])!=mp.end())
-        //         {
-        //             mp[s[i]]++;
-        //             if(mp[s[i]]==1){
-        //             cnt++;
-        //             }
-        //         }
-        //         i++,j++;
-        //     }
-        // }
-        // return v;
-        // -----------------------
-        // unordered_map<char,int>mp;
-        // vector<int>ans;
-        // for(int i=0;i<p.length();i++)
-        // {
-        //     mp[p[i]]++;
-        //  }
-        // int cnt=mp.size();
-        // int i=0,j=0,k=p.size();
-        // while(j<s.length())
-        // {
-        //     if(mp.find(s[j])!=mp.end())
-        //     {
-        //         mp[s[j]]--;
-        //         if(mp[s[j]]==0)
-        //             cnt--;
-        //     }
-        //     if(j-i+1<k)
-        //     {
-        //         j++;
-        //     }
-        //     else if(j-i+1==k)
-        //     {
-        //         if(cnt==0)
-        //         {
-        //             ans.push_back(i);
-        //         }
-        //         if(mp.find(s[i])!=mp.end())
-        //         {
-        //             mp[s[i]]++;
-        //             if(mp[s[i]]==1)
-        //             {
-        //                 cnt++;
-        //             }
-        //         }
-        //         i++,j++;
-        //     }
-        // }
-        // return ans;
-        // -------------
+        int k=p.length();
+        int n=s.length();
         unordered_map<char,int>mp;
-        vector<int>ans;
-        for(int i=0;i<p.length();i++)
+        vector<int>v;
+        for(auto ch:p)
         {
-            mp[p[i]]++;
+            mp[ch]++;
         }
         int cnt=mp.size();
-        int i=0,j=0,k=p.size();
-        while(j<s.length())
+        int i=0,j=0;
+        while(j<n)
         {
             if(mp.find(s[j])!=mp.end())
             {
@@ -95,64 +21,28 @@ public:
                     cnt--;
                 }
             }
-           if(j-i+1<k)
+            if(j-i+1<k)
             {
                 j++;
             }
-            else if(j-i+1==k)
+             else if(j-i+1==k)
             {
                 if(cnt==0)
                 {
-                    ans.push_back(i);
+                    v.push_back(i);
                 }
-                if(mp.find(s[i])!=mp.end())
-                {
-                    mp[s[i]]++;
-                    if(mp[s[i]]==1)
-                    {
-                        cnt++;
-                    }
-                }
-                i++,j++;
+                 if(mp.find(s[i])!=mp.end())
+                 {
+                     mp[s[i]]++;
+                     if(mp[s[i]]==1)
+                     {
+                         cnt++;
+                     }
+                 }
+                 i++,j++;
             }
         }
-        return ans;
-            
+        return v;
         
-    
-        
-//         unordered_map<char,int>m;
-        
-//         vector<int>v;
-        
-//         for(int i=0;i<p.size();i++)m[p[i]]++;
-        
-//         int count=m.size(),i=0,j=0,k=p.size();
-        
-//         while(j<s.size()){
-//             if(m.find(s[j])!=m.end()){
-//                 m[s[j]]--;
-//                 if(m[s[j]]==0)count--;
-//             }
-            
-//             if(j-i+1<k) j++;
-            
-//             else if(j-i+1==k){
-//                 if(count==0)v.push_back(i);
-                
-//                 if(m.find(s[i])!=m.end()){
-//                     m[s[i]]++;
-//                     if(m[s[i]]==1)count++;
-//                 }
-//                 i++;
-//                 j++;
-//             }
-//         }
-//         return v;
-    
-
-
-
-
     }
 };
