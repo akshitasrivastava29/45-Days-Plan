47698262109a2c223bc2e775a1890afe5ee11302
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-10 09:54:50 +0530

    Time: 11 ms (63.91%), Space: 14.7 MB (81.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,110 +11,13 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       //  ListNode *p1=list1;
-       //  ListNode*p2=list2;
-       //  ListNode*dummynode=new ListNode(-1);
-       //  ListNode*p3=dummynode;
-       // while(p1!=NULL && p2!=NULL)
-       // {
-       //      if(p1->val<=p2->val)
-       //      {
-       //          p3->next=p1;
-       //          p1=p1->next;
-       //      }
-       //     else
-       //     {
-       //         p3->next=p2;
-       //         p2=p2->next;
-       //     }
-       //     p3=p3->next;
-       // }
-       //  while(p1!=NULL)
-       //  {
-       //      p3->next=p1;
-       //      p1=p1->next;
-       //      p3=p3->next;
-       //  }
-       //  while(p2!=NULL)
-       //  {
-       //      p3->next=p2;
-       //      p3=p3->next;
-       //      p2=p2->next;
-       //   }
-       //  return dummynode->next;
-        // ____________________________
-        // ListNode*p1=list1;
-//         ListNode*p2=list2;
-//         ListNode*dummy=new ListNode(-1);
-//         ListNode*p3=dummy;
-//         while(p1!=NULL && p2!=NULL)
-//         {
-//             if(p1->val<p2->val)
-//             {
-//                 p3->next=p1;
-//                 p1=p1->next;
-//             }
-//             else
-//             {
-//                 p3->next=p2;
-//                 p2=p2->next;
-//             }
-//             p3=p3->next;
-//         }
-//         while(p1!=NULL)
-//         {
-//             p3->next=p1;
-//             p1=p1->next;
-//             p3=p3->next;
-//         }
-//         while(p2!=NULL)
-//         {
-//             p3->next=p2;
-//             p2=p2->next;
-//             p3=p3->next;
-//         }
-        
-//       return dummy->next; 
-        // ____________________
-        // ListNode*p1=list1;
-        // ListNode*p2=list2;
-        // ListNode*dummy=new ListNode(-1);
-        // ListNode*p3=dummy;
-        // while(p1!=NULL && p2!=NULL)
-        // {
-        //     if(p1->val<p2->val)
-        //     {
-        //         p3->next=p1;
-        //         p1=p1->next;
-        //     }
-        //     else
-        //     {
-        //         p3->next=p2;
-        //         p2=p2->next;
-        //     }
-        //     p3=p3->next;
-        // }
-        // while(p1!=NULL)
-        // {
-        //     p3->next=p1;
-        //     p1=p1->next;
-        //     p3=p3->next;
-        // }
-        // while(p2!=NULL)
-        // {
-        //     p3->next=p2;
-        //     p2=p2->next;
-        //     p3=p3->next;
-        // }
-        // return dummy->next;
-        // // _______________---
+        ListNode *dummy=new ListNode();
         ListNode*p1=list1;
         ListNode*p2=list2;
-        ListNode*dummy=new ListNode(-1);
         ListNode*p3=dummy;
         while(p1!=NULL && p2!=NULL)
         {
-            if(p1->val<=p2->val)
+            if(p1->val <=p2->val)
             {
                 p3->next=p1;
                 p1=p1->next;
@@ -128,7 +31,7 @@ public:
         }
         while(p1!=NULL)
         {
-            p3->next=p1;
+           p3->next=p1;
             p1=p1->next;
             p3=p3->next;
         }
@@ -138,6 +41,6 @@ public:
             p2=p2->next;
             p3=p3->next;
         }
-    return dummy->next;
+       return dummy->next; 
     }
 };
