babf930799dfe8966a5c70095055c47a28176ead
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-14 22:12:04 +0530

    Time: 37 ms (63.23%), Space: 13.4 MB (60.31%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,86 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class comp
-{
-    public:
-    bool operator()(const ListNode*a,const ListNode*b )
+ class comp
     {
-        return (a->val >b->val);
-    }
-};
+        public:
+        bool operator()(const ListNode *a, const ListNode*b)
+        {
+            return (a->val>b->val);
+        }
+    };
 class Solution {
-
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // priority_queue<int ,vector<int>,greater<int>>pq;
-        // for(int i=0;i<lists.size();i++)
-        // {
-        //     ListNode*head=lists[i];
-        //     while(head!=NULL)
-        //     {
-        //         pq.push(head->val);
-        //         head=head->next;
-        //     }
-        // }
-        // ListNode*start=NULL;
-        // ListNode*end=NULL;
-        // while(!pq.empty())
-        // {
-        //     if(start==NULL)
-        //     {
-        //         if(!pq.empty())
-        //         start=new ListNode(pq.top());
-        //         end=start;
-        //         pq.pop();
-        //     }
-        //     else
-        //     {
-        //         end->next=new ListNode(pq.top());
-        //         pq.pop();
-        //         end=end->next;
-        //     }
-        // }
-        // return start;
-        
-        
-    // priority_queue<int ,vector<int>,greater<int>>pq;
-    //     for(int i=0;i<lists.size();i++)
-    //     {
-    //         ListNode*temp=lists[i];
-    //         while(temp!=NULL)
-    //         {
-    //             pq.push(temp->val);
-    //             temp=temp->next;
-    //         }
-    //     }
-    //     ListNode*head=NULL;
-    //     ListNode*tail=NULL;
-    //     while(!pq.empty())
-    //     {
-    //         if(head==NULL)
-    //         {
-    //             head=new ListNode(pq.top());
-    //             tail=head;
-    //             pq.pop();
-    //         }
-    //         else
-    //         {
-    //             tail->next=new ListNode(pq.top());
-    //             pq.pop();
-    //             tail=tail->next;
-    //         }
-    //     }
-        // return head;
-        
-        ListNode*dummy=new ListNode(0);
+        int k=lists.size();
+        ListNode*dummy=new ListNode(-1);
         ListNode*temp=dummy;
-        priority_queue<ListNode*,vector<ListNode*>,comp>pq;
-        for(int i=0;i<lists.size();i++)
+        priority_queue<ListNode*,vector<ListNode*>,comp>pq;   //minheap
+        for(int i=0;i<k;i++)
         {
             if(lists[i]!=NULL)
             {
-                pq.push(lists[i]);
+            pq.push(lists[i]);
             }
         }
         while(!pq.empty())
