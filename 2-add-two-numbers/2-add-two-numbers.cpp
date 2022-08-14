ce8376a0d1ead23efa0cce4dce947af216a0f4db
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-14 21:35:10 +0530

    Time: 37 ms (89.12%), Space: 71.5 MB (12.23%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,119 +11,12 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode*p1=l1;
-//         ListNode*p2=l2;
-//         ListNode*dummynode=new ListNode();
-//         ListNode*p3=dummynode;
-//         int carry=0;
-        
-//         while(p1!=NULL||  p2!=NULL||carry)
-//         {
-//             int sum=0;
-//              if(p1!=NULL)
-//              {
-//                  sum+=p1->val;
-//                  p1=p1->next;
-//              }
-//             if(p2!=NULL)
-//             {
-//                 sum+=p2->val;
-//                 p2=p2->next;
-//             }
-//             sum+=carry;
-//             carry=sum/10;
-//             ListNode*newnode=new ListNode(sum%10);
-//             p3->next=newnode;
-//             p3=p3->next;
-            
-//         }
-        
-//        return dummynode->next; 
-        // _________________________
-        // ListNode*p1=l1;
-        // ListNode*p2=l2;
-        // ListNode*dummy=new ListNode(-1);
-        // ListNode*p3=dummy;
-        // int carry;
-        // while(p1!=NULL || p2!=NULL || carry)
-        // {
-        //     int sum=0;
-        //     if(p1!=NULL)
-        //     {
-        //         sum+=p1->val;
-        //         p1=p1->next;
-        //     }
-        //     if(p2!=NULL)
-        //     {
-        //         sum+=p2->val;
-        //         p2=p2->next;
-        //     }
-        //     sum+=carry;
-        //     carry=sum/10;
-        //     ListNode*newnode=new ListNode(sum%10);
-        //     p3->next=newnode;
-        //     p3=p3->next;
-        // }
-        // return dummy->next;
-        // ________________________
-//         ListNode*dummy=new ListNode(-1);
-//         ListNode*p1=l1;
-//         ListNode*p2=l2;
-//         ListNode*temp=dummy;
-//         int carry;
-//         while(p1!=NULL || p2!=NULL || carry)
-//         {
-//             int sum=0;
-//             if(p1!=NULL)
-//             {
-//                 sum+=p1->val;
-//                 p1=p1->next;
-//             }
-//             if(p2!=NULL)
-//             {
-//                 sum+=p2->val;
-//                 p2=p2->next;
-//             }
-//             sum+=carry;
-//             carry=sum/10;
-//             ListNode*newnode=new ListNode(sum%10);
-//             temp->next=newnode;
-//             temp=temp->next;
-            
-//         }
-//         return dummy->next;
-        // ______________------
-        // ListNode*dummy=new ListNode(-1);
-        // ListNode*p1=l1;
-        // ListNode*p2=l2;
-        // ListNode*temp=dummy;
-        // int carry=0;
-        // while(p1!=NULL || p2!=NULL || carry)
-        // {
-        //     int sum=0;
-        //     if(p1!=NULL)
-        //     {
-        //         sum+=p1->val;
-        //         p1=p1->next;
-        //     }
-        //     if(p2!=NULL){
-        //         sum+=p2->val;
-        //         p2=p2->next;
-        //     }
-        //     sum+=carry;
-        //     carry=sum/10;
-        //     ListNode*newnode=new ListNode(sum%10);
-        //     temp->next=newnode;
-        //     temp=temp->next;
-        // }
-        // return dummy->next;
-        // ----------------
-        ListNode*dummy=new ListNode(-1);
+        ListNode*dummy =new ListNode(-1);
         ListNode*p1=l1;
         ListNode*p2=l2;
         ListNode*p3=dummy;
         int carry=0;
-        while(p1!=NULL || p2!=NULL || carry)
+        while(p1!=NULL || p2!=NULL  || carry)
         {
             int sum=0;
             if(p1!=NULL)
@@ -138,10 +31,11 @@ public:
             }
             sum+=carry;
             carry=sum/10;
-            ListNode*newnode=new ListNode(sum%10);
+            ListNode* newnode=new ListNode(sum%10);
             p3->next=newnode;
             p3=p3->next;
         }
-        return dummy->next;
-     }
+          return dummy->next;
+
+    }
 };
