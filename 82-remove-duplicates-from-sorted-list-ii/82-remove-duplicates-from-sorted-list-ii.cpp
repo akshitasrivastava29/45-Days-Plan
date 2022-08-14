d67fbbe5e98239200ce3f9a36d730b8d42db859f
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-14 23:05:56 +0530

    Time: 10 ms (73.61%), Space: 11.2 MB (58.77%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -10,83 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* &head) {
-    //  ListNode*prevptr=NULL;
-    // ListNode*currptr=head;
-    //  ListNode*nextptr=head->next;
-    //     if(head==NULL || head->next==NULL)
-    //     {
-    //         return head;
-    //     }
-    //     while(nextptr)
-    //     {
-    //          if(nextptr!=NULL && currptr->val ==nextptr->val)
-    //          {
-    //              while(nextptr && currptr->val==nextptr->val)
-    //                 {
-    //                     nextptr=nextptr->next;
-    //                  }
-    //              if(prevptr==NULL)
-    //                  {
-    //                    head=nextptr;
-    //                 }
-    //             else
-    //                 {
-    //               prevptr->next=nextptr;
-    //                  }
-    //          }
-    //     else
-    //          {
-    //              prevptr=currptr;
-    //          }
-    //     currptr=nextptr;
-    //             if(nextptr!=NULL)
-    //         {
-    //              nextptr=nextptr->next;
-    //          }
-    //     }
-    //     return head;
+    ListNode* deleteDuplicates(ListNode* head) {
         
-        
-//         if(head==NULL)
-//             return  0;
-//         if(head->next==NULL)
-//         {
-//             return head;
-//         }
-//         int val=head->val;
-//         ListNode*temp=head->next;
-//         if(temp->val!=val)
-//         {
-//             head->next=deleteDuplicates(temp);
-//             return head;
-//         }
-//         else
-//         {
-//             while(temp&&temp->val==val)
-//             {
-//                 ListNode*del=temp;
-//                 temp=temp->next;
-//                 delete del;
-//             }
-//             return deleteDuplicates(temp);
-            
-//         }
-        
-        // if(head==NULL || head->next==NULL)
-        // {
-        //     return head;
-        // }
-         ListNode*d=new ListNode(0,head);
+        ListNode*d =new ListNode(0,head);
         ListNode*prev=d;
-        
         while(head!=NULL)
         {
             if(head->next!=NULL && head->val==head->next->val)
             {
-                while( head->next!=NULL && head->val==head->next->val)
+                while(head->next!=NULL && head->val == head->next->val)
                 {
-                head=head->next;
+                    head=head->next;
                 }
                 prev->next=head->next;
             }
@@ -97,26 +31,5 @@ public:
             head=head->next;
         }
         return d->next;
-       
-        
-        // if(head->val!=head->next->val)
-        // {
-        //     head=head->next;
-        //     prev=prev->next;
-        // }
-        // else if(head->val==head->next->val)
-        // {
-        //     while(head->val!=head->next->val)
-        //     {
-        //         head=head->next;
-        //     }
-        //     prev->next=head->next;
-        //     head=head->next;
-        // }
-        
-            
-
-        
-        
     }
 };
