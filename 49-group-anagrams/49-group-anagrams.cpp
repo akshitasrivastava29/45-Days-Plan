4a0f18991a81903baba491c020ca0ff861854098
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-15 19:58:52 +0530

    Time: 93 ms (18.00%), Space: 20.7 MB (55.58%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -15,20 +15,52 @@ public:
     //         res.push_back(it.second());
     //     }
     //     return res;
-    unordered_map<string,vector<string>>mp;
-        int n=strs.size();
-        string temp="";
-        for(int i=0;i<n;i++)
-        {
-            temp=strs[i];
-            sort(strs[i].begin(),strs[i].end());
-            mp[strs[i]].push_back(temp);
-        }
-        vector<vector<string >>res;
-        for(auto it:mp)
-        {
-            res.push_back(it.second);
-        }
-        return res;
+         
+         // -----------------
+    // unordered_map<string,vector<string>>mp;
+    //     int n=strs.size();
+    //     string temp="";
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         temp=strs[i];
+    //         sort(strs[i].begin(),strs[i].end());
+    //         mp[strs[i]].push_back(temp);
+    //     }
+    //     vector<vector<string >>res;
+    //     for(auto it:mp)
+    //     {
+    //         res.push_back(it.second);
+    //     }
+    //     return res;
+         // -----------------------
+         // unordered_map<string,vector<string>>mp;
+         // string temp="";
+         // for(int i=0;i<strs.size();i++)
+         // {
+         //     temp=strs[i];
+         //     sort(strs[i].begin(),strs[i].end());
+         //     mp[strs[i]].push_back(temp);
+         // }
+         // vector<vector<string>>v;
+         // for(auto it:mp)
+         //     {
+         //         v.push_back(it.second);
+         //     }
+         // return v;
+         // ---------------------
+         unordered_map<string,vector<string>>mp;
+         string temp="";
+         for(int i=0;i<strs.size();i++)
+         {
+             temp=strs[i];
+             sort(strs[i].begin(),strs[i].end());
+             mp[strs[i]].push_back(temp);
+         }
+         vector<vector<string>>v;
+         for(auto it:mp)
+         {
+             v.push_back(it.second);
+         }
+         return v;
     }
 };
