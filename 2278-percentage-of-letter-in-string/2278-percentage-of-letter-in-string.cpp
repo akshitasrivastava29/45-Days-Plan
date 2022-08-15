8e178081d9ae50262e40ffb95579e316eba15796
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-15 18:30:19 +0530

    Time: 3 ms (41.29%), Space: 6.4 MB (9.59%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int percentageLetter(string s, char letter) {
-       //  int n=s.length();
-       //  int ans=0;
-       //  unordered_map<char,int>mp;
-       //  for(auto ch:s)
-       //  {
-       //      mp[s[ch]]++;
-       //  }
-       //  for(auto it:mp)
-       //  {
-       //      if(mp.find(letter)!=mp.end())
-       //      {
-       //          ans=it.second/2*100;
-       //      }
-       //      else
-       //      {
-       //      ans=0;
-       //      }
-       //  }
-       // return ans; 
-        
         int n=s.length();
-        int temp=count(s.begin(),s.end(),letter);
-        return (temp*100)/n;
+        int ans=0;
+        unordered_map<char,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.first==letter)
+            {
+                ans=(it.second*100/n);
+            }
+        }
+       return ans; 
+        
+        // int n=s.length();
+        // int temp=count(s.begin(),s.end(),letter);
+        // return (temp*100)/n;
     }
 };
