a234a0b2c96d4e594d93685bdaa3ea239a564702
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-19 18:40:53 +0530

    Time: 52 ms (70.75%), Space: 33.6 MB (13.05%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -10,55 +10,37 @@
  * };
  */
 class Solution {
-    // void CreateTree(TreeNode* &root,TreeNode* root1,TreeNode* root2)
-    // {
-    //     if(root1==NULL && root2==NULL)root=NULL ;
-    //     else if(root1==NULL && root2!=NULL){
-    //         root=new TreeNode(root2->val);
-    //         CreateTree(root->left,NULL,root2->left);
-    //         CreateTree(root->right,NULL,root2->right);
-    //     }
-    //     else if(root1!=NULL && root2==NULL)
-    //     {
-    //         root=new TreeNode(root1->val);
-    //         CreateTree(root->left,root1->left,NULL);
-    //         CreateTree(root->right,root1->right,NULL);
-    //     }
-    //     else
-    //     {
-    //         root=new TreeNode(root1->val+root2->val);
-    //         CreateTree(root->left,root1->left,root2->left);
-    //         CreateTree(root->right,root1->right,root2->right);
-    //     }
-    // }
-    void maketree(TreeNode*&root ,TreeNode* root1, TreeNode* root2)
+    void createTree(TreeNode*&root,TreeNode* root1, TreeNode* root2)
     {
         if(root1==NULL && root2==NULL)
-            root=NULL;
-        else if(root1==NULL && root2!=NULL)
         {
-            root= new TreeNode(root2->val);
-            maketree(root->left,NULL,root2->left);
-            maketree(root->right,NULL,root2->right);
-            
+            root=NULL;
         }
         else if(root1!=NULL && root2==NULL)
         {
-            root= new TreeNode(root1->val);
-            maketree(root->left,root1->left,NULL);
-            maketree(root->right,root1->right,NULL);
+            root=new TreeNode(root1->val);
+            createTree(root->left,root1->left,NULL);
+            createTree(root->right,root1->right,NULL);
+            
+        }
+        else if(root1==NULL && root2!=NULL)
+        {
+            root=new TreeNode(root2->val);
+            createTree(root->left,NULL,root2->left);
+            createTree(root->right,NULL,root2->right);
         }
         else
         {
-            root= new TreeNode(root2->val+root1->val);
-            maketree(root->left,root1->left,root2->left);
-            maketree(root->right,root1->right,root2->right);
+            root=new  TreeNode(root1->val + root2->val);
+            createTree(root->left,root1->left,root2->left);
+            createTree(root->right,root1->right,root2->right);
         }
     }
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         TreeNode*root;
-        maketree(root,root1,root2);
+        createTree(root,root1,root2);
         return root;
+        
     }
 };
