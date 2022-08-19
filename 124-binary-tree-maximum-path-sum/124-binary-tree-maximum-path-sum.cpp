a844927876ae3b0a01c148c4698957d91a23125a
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-19 18:48:30 +0530

    Time: 31 ms (80.97%), Space: 27.6 MB (92.03%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -10,50 +10,17 @@
  * };
  */
 class Solution {
-    // private:
-    // int maxPathDown(TreeNode*node,int&maxi)
-    // {
-    //     if(node==NULL)
-    //     {
-    //         return 0;
-    //     }
-    //     int left=max(0,maxPathDown(node->left,maxi));
-    //     int right=max(0,maxPathDown(node->right,maxi));
-    //     maxi=max(maxi,left+right+node->val);
-    //     return max(left ,right)+node->val;
-    // }
-    private:
-    // int maxpathdown(TreeNode*node,int &maxi)
-    // {
-    //     if(node==NULL)
-    //         return 0;
-    //     int left=max(0,maxpathdown(node->left,maxi));
-    //     int right=max(0,maxpathdown(node->right,maxi));
-    //     maxi=max(maxi,left+right+node->val);
-    //     return max(left,right)+node->val;
-    // }
-    
-    // --------
-    int maxpath(TreeNode*node,int &maxi)
+    int maxpath(TreeNode* root,int &maxi)
     {
-        if(node==NULL)return 0;
-        int left=max(0,maxpath(node->left, maxi));
-        int right=max(0,maxpath(node->right,maxi));
-        maxi=max(maxi,left+right+node->val);
-        return max(left,right)+node->val;
+        if(root==NULL)
+            return 0;
+        int left=max(0,maxpath(root->left,maxi));
+        int right=max(0,maxpath(root->right,maxi));
+        maxi=max(maxi,left+right+root->val);
+        return max(left,right)+root->val;
     }
 public:
     int maxPathSum(TreeNode* root) {
-        // int maxi=INT_MIN;
-        // maxPathDown(root,maxi);
-        // return maxi;
-        
-        
-        
-        // int maxi=INT_MIN;
-        // maxpathdown(root,maxi);
-        // return maxi;
-        
         int maxi=INT_MIN;
         maxpath(root,maxi);
         return maxi;
