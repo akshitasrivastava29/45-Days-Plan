f80f9d8f8bbeef786cec3287952d0879ffce15da
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-23 01:48:16 +0530

    Time: 60 ms (74.56%), Space: 18.8 MB (73.74%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,69 +1,29 @@
 class Solution {
     private:
-    int fun(vector<int>&piles,int perhr,int h)
+    int fun(vector<int>&piles,int ph,int h)
     {
         int n=piles.size();
         long long cnt=0;
         for(int i=0;i<n;i++)
         {
-            cnt+=piles[i]/perhr;
-            if(piles[i]%perhr)
+            cnt+=piles[i]/ph;
+            if(piles[i]%ph!=0)
             {
                 cnt+=1;
             }
         }
-        return cnt<=h;
+        return (cnt<=h);
     }
-    // private:
-//     int fun(vector<int>&piles,int perhr,int h)
-//     {
-//         int n=piles.size();
-//         int m=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             m+=piles[i]/perhr;
-//             if(piles[i]%perhr)
-//             {
-//                 m+=1;
-//             }
-//         }
-        
-//      return m<=h;
-//     }
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-      //   int n=piles.size();
-      //   int low=1;
-      //   int maxi=piles[0];
-      //   int ans;
-      //   for(int i=0;i<n;i++)
-      //   {
-      //       maxi=max(maxi,piles[i]);
-      //   }
-      //   int high=maxi;
-      //   while(low<=high)
-      //   {
-      //       int mid=low+(high-low)/2;
-      //       if(fun(piles,mid,h))
-      //       {
-      //           ans=mid;
-      //           high=mid-1;
-      //       }
-      //       else
-      //       {
-      //           low=mid+1;
-      //       }
-      //   }
-      // return ans;
         int n=piles.size();
-        int low=1;
         int maxi=piles[0];
         int ans;
         for(int i=0;i<n;i++)
         {
             maxi=max(maxi,piles[i]);
         }
-        int high=maxi;
+        int low=1,high=maxi;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
@@ -76,6 +36,7 @@ public:
             {
                 low=mid+1;
             }
+            
         }
         return ans;
         
