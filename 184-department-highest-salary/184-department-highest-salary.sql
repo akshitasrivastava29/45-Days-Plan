dbf1c8cd6473efcc53527cf1cc4b9e6eb46d1733
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-25 23:54:48 +0530

    Time: 1012 ms (38.40%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,13 +1,5 @@
 # Write your MySQL query statement below
-
-
-# SELECT t1.Department,t1.Employee,t1.salary
-# FROM (SELECT d.name as Department,e.name as Employee,e.salary as salary,RANK() OVER(PARTITION BY d.id ORDER BY salary desc ) as RK 
-# FROM DEPARTMENT as d
-# JOIN  Employee as e ON  e.departmentId=d.id) as t1 where rk=1
-
-SELECT d.name Department,e.name Employee,e.salary salary
-FROM  Employee e JOIN Department d
-ON e.departmentId=d.id
-WHERE (salary,departmentId)IN (select max(salary),departmentId from Employee group by departmentId);
-
+SELECT t1.Department,t1.Employee,t1.salary
+FROM (SELECT d.name as Department,e.name as Employee,e.salary as salary,RANK() OVER(PARTITION BY d.id ORDER BY salary desc ) as RK 
+FROM DEPARTMENT as d
+JOIN  Employee as e ON  e.departmentId=d.id) as t1 where rk=1
