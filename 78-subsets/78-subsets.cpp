2ec885f97eecb7f2b550e5913600cdd4a2fa7356
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-27 22:33:02 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.56%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,35 +1,4 @@
 class Solution {
-    private:
-//     void fun(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&v)
-//     {
-//         int n=nums.size();
-//         if(ind==n)
-//         {
-//             v.push_back(temp);
-//             return ;
-//         }
-//         temp.push_back(nums[ind]);
-//         fun(ind+1,nums,temp,v);
-//         temp.pop_back();
-//         fun(ind+1,nums,temp,v);
-        
-//     }
-    // -------------
-//     void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
-//     {
-//         int n=nums.size();
-//         if(ind==n)
-//         {
-//             ans.push_back(temp);
-//             return ;
-//         }
-//         temp.push_back(nums[ind]);
-//         getsubsets(ind+1,nums,temp,ans);
-//         temp.pop_back();
-//         getsubsets(ind+1,nums,temp,ans);
-        
-//     }
-    // -------
     void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
     {
         if(ind==nums.size())
@@ -41,16 +10,9 @@ class Solution {
         getsubsets(ind+1,nums,temp,ans);
         temp.pop_back();
         getsubsets(ind+1,nums,temp,ans);
-        
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        // int n=nums.size();
-        // vector<int>temp;
-        // vector<vector<int>>v;
-        // fun(0,nums,temp,v);
-        // return v;
-        
         int n=nums.size();
         vector<int>temp;
         vector<vector<int>>ans;
