aaad48f6db37069f07039694221ce56c32accaee
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-28 00:01:55 +0530

    Time: 3 ms (95.40%), Space: 10.7 MB (47.47%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,49 +1,15 @@
 class Solution {
-    private:
-//     void fun(int ind,int target,vector<int>&arr,vector<int>&s,vector<vector<int>>&ans)
-//     {
-//         int n=arr.size();
-//         if(target==0)
-//         {
-//             ans.push_back(s);
-//             return;
-//         }
-// //         if(i>=n)
-// //             return ;
-// //         for(int i=0;i<n;i++)
-// //         {
-// //             if(arr[i]>target)
-// //             {
-// //                 break;
-// //             }
-            
-// //             s.push_back(arr[i]);
-// //             fun(i+1,target-arr[i],s,arr,ans);
-// //             s.pop_back();
-// //         }
-//         for(int i=ind;i<arr.size();i++)
-//         {
-//             if(i>ind && arr[i]==arr[i-1] )
-//                 continue;
-//             if(arr[i]>target)
-//                 break; 
-//             s.push_back(arr[i]);
-//             fun(i+1,target-arr[i],arr,s,ans);
-//             s.pop_back();
-//         }
-//     }
-    void getcombo(int ind,vector<int>&candidates,vector<int>&ds,vector<vector<int>>&ans,int target)
+    void fun(int ind,vector<int>&temp,vector<vector<int>>&ans,vector<int>& candidates,int target)
     {
         int n=candidates.size();
-            if(target==0)
-            {
-                ans.push_back(ds);
-                return ;
-            }
-        
+        if(target==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
         for(int i=ind;i<n;i++)
         {
-            if(i>ind && candidates[i]==candidates[i-1])
+            if(i>ind &&  candidates[i]==candidates[i-1])
             {
                 continue;
             }
@@ -51,23 +17,21 @@ class Solution {
             {
                 break;
             }
-            ds.push_back(candidates[i]);
-            getcombo(i+1,candidates,ds,ans,target-candidates[i]);
-            ds.pop_back();
+            temp.push_back(candidates[i]);
+            fun(i+1,temp,ans,candidates,target-candidates[i]);
+            temp.pop_back();
+            
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        // sort(candidates.begin(),candidates.end());
-        // vector<vector<int>>ans;
-        // vector<int>s;
-        // fun(0,target,candidates,s,ans);
-        // return ans;
-        // ----------
+        int n=candidates.size();
         sort(candidates.begin(),candidates.end());
-        vector<int>ds;
+        vector<int>temp;
         vector<vector<int>>ans;
-        getcombo(0,candidates,ds,ans,target);
+        fun(0,temp,ans,candidates,target);
         return ans;
+        
+        
     }
 };
