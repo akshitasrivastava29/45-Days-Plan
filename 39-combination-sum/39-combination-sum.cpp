94e32c4a21a9ab87bacf0f6bd080678168dfbb43
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-27 23:05:07 +0530

    Time: 217 ms (14.21%), Space: 52.4 MB (18.01%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,59 +1,32 @@
 class Solution {
-    private:
-//     void fun(int i,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans,int target)
-//     {
-//         int n=nums.size();
-//         if(i==n)
-//         {
-//             if(target==0)
-//             {
-//                 ans.push_back(ds);
-//             }
-//             return ;
-//         }
-        
-//     if(nums[i]<=target)
-//     {
-//         ds.push_back(nums[i]);
-//             fun(i,nums,ds,ans,target-nums[i]);
-//         ds.pop_back();
-//     }
-//             fun(i+1,nums,ds,ans,target);
-    
-// }
-    void getcombo(int ind,vector<int>&candidates, vector<int>&ds,vector<vector<int>>&ans,int target)
+    void fun(int ind,vector<int>&candidates,int target,vector<int>temp,vector<vector<int>>&ans)
     {
-        int n=candidates.size();
-        if(ind==n)
+        if(ind==candidates.size())
         {
             if(target==0)
             {
-                ans.push_back(ds);
+            ans.push_back(temp);
             }
-            return ;
+            return;
         }
         if(candidates[ind]<=target)
         {
-            ds.push_back(candidates[ind]);
-            getcombo(ind,candidates,ds,ans,target-candidates[ind]);
-            ds.pop_back();
+            temp.push_back(candidates[ind]);
+            fun(ind,candidates,target-candidates[ind],temp,ans);
+            temp.pop_back();
         }
-            getcombo(ind+1,candidates,ds,ans,target);
+            fun(ind+1,candidates,target,temp,ans);
         
     }
-    
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-//         vector<int>ds;
-//         vector<vector<int>>ans;
-//         fun(0,candidates,ds,ans,target);
-        
-//         return ans;
-        
-        
-        vector<int>ds;
+        int n=candidates.size();
+        vector<int>temp;
         vector<vector<int>>ans;
-        getcombo(0,candidates,ds,ans,target);
+        fun(0,candidates,target,temp,ans);
         return ans;
+        
+        
+        
     }
 };
