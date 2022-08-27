591f87c8232ab8eb0e04afc4ccf89bfb4e9f5a0e
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-27 22:46:18 +0530

    Time: 3 ms (74.45%), Space: 7.2 MB (57.04%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,41 @@
 class Solution {
-    void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
-    {
-        if(ind==nums.size())
-        {
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[ind]);
-        getsubsets(ind+1,nums,temp,ans);
-        temp.pop_back();
-        getsubsets(ind+1,nums,temp,ans);
-    }
+//     void getsubsets(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans)
+//     {
+// //         if(ind==nums.size())
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+//         temp.push_back(nums[ind]);
+//         getsubsets(ind+1,nums,temp,ans);
+//         temp.pop_back();
+//         getsubsets(ind+1,nums,temp,ans);
+//     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>temp;
-        vector<vector<int>>ans;
-        getsubsets(0,nums,temp,ans);
-        return ans;
+//         int n=nums.size();
+//         vector<int>temp;
+//         vector<vector<int>>ans;
+//         getsubsets(0,nums,temp,ans);
+//         return ans;
         
+        //power set with bit manipulation
+        
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int num=0;num<(1<<n);num++)
+        {
+            vector<int>temp;
+            for(int i=0;i<n;i++)
+            {
+                if(num & (1<<i))
+                {
+                    temp.push_back(nums[i]);
+                }
+            }
+            ans.push_back(temp);
+        }
+     return ans;   
         
     }
 };
