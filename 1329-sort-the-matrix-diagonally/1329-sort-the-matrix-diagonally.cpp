bc9bb4481ae0d71473b5dbb5a772d46bf70a8c53
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-28 21:58:21 +0530

    Time: 19 ms (49.33%), Space: 9.4 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,5 +1,4 @@
 class Solution {
-    private:
     void fun(int i,int j,vector<vector<int>>&mat)
     {
         int n=mat.size();
@@ -12,25 +11,25 @@ class Solution {
             ti++,tj++;
         }
         sort(temp.begin(),temp.end());
-        for(auto x:temp)
+        for(auto it:temp)
         {
-            mat[i][j]=x;
+            mat[i][j]=it;
             i++,j++;
         }
-        
     }
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        if(n==1 || m==1)return mat;
-        for(int i=0;i<m;i++){
-            fun(0,i,mat);
+        if(n==1 || m==1) return mat;
+        for(int j=0;j<m;j++)
+        {
+            fun(0,j,mat);
         }
         for(int i=1;i<n;i++)
         {
             fun(i,0,mat);
         }
-       return mat; 
+      return mat;  
     }
 };
