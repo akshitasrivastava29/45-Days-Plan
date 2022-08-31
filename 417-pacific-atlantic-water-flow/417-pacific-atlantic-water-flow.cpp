5895f81fc8b464acfce8968ca97d0fcb0cba60af
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-08-31 18:45:49 +0530

    Time: 57 ms (74.75%), Space: 19 MB (49.23%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,13 +1,32 @@
 class Solution {
     private:
-    void dfs(vector<vector<int>>& heights,int r ,int c ,int prev,vector<vector<int>>&ocean)
+    // void dfs(vector<vector<int>>& heights,int r ,int c ,int prev,vector<vector<int>>&ocean)
+    // {
+    //     int n=heights.size();
+    //     int m=heights[0].size();
+    //     if(r<0 || c<0 || r>=n || c>=m) return ;
+    //     if(heights[r][c]<prev)
+    //     {
+    //         return;
+    //     }
+    //     if(ocean[r][c]==-1)
+    //     {
+    //         return;
+    //     }
+    //     ocean[r][c]=-1;
+    //     dfs(heights,r+1,c,heights[r][c],ocean);
+    //     dfs(heights,r-1,c,heights[r][c],ocean);
+    //     dfs(heights,r,c+1,heights[r][c],ocean);
+    //     dfs(heights,r,c-1,heights[r][c],ocean);
+    // }
+    void dfs(vector<vector<int>>&heights,int r,int c,int prev,vector<vector<int>>&ocean)
     {
         int n=heights.size();
         int m=heights[0].size();
-        if(r<0 || c<0 || r>=n || c>=m) return ;
+        if(r<0 || c<0 || r>=n||c>=m) return ;
         if(heights[r][c]<prev)
         {
-            return;
+            return ;
         }
         if(ocean[r][c]==-1)
         {
@@ -21,13 +40,46 @@ class Solution {
     }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+//         int n=heights.size();
+//         int m=heights[0].size();
+//         vector<vector<int>>ans;
+//         if(n==0)
+//         {
+//             return  ans;
+//         }
+//         vector<vector<int>>pac(n,vector<int>(m,0));
+//         vector<vector<int>>atl(n,vector<int>(m,0));
+//         for(int c=0;c<m;c++)
+//         {
+//             dfs(heights,0,c,INT_MIN,pac);
+//             dfs(heights,n-1,c,INT_MIN,atl);
+//         }
+//         for(int r=0;r<n;r++)
+//         {
+//             dfs(heights, r,0,INT_MIN,pac);
+//             dfs(heights,r,m-1,INT_MIN,atl);
+//         }
+//         vector<int>v;
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<m;j++)
+//             {
+//                 if(pac[i][j]==atl[i][j] && pac[i][j]==-1)
+//                 {
+//                     v.clear();
+//                     v.push_back(i);
+//                     v.push_back(j);
+//                     ans.push_back(v);
+                    
+//                 }
+//             }
+//         }
+        
+//         return ans;
         int n=heights.size();
         int m=heights[0].size();
         vector<vector<int>>ans;
-        if(n==0)
-        {
-            return  ans;
-        }
+        if(n==0) return ans;
         vector<vector<int>>pac(n,vector<int>(m,0));
         vector<vector<int>>atl(n,vector<int>(m,0));
         for(int c=0;c<m;c++)
@@ -37,7 +89,7 @@ public:
         }
         for(int r=0;r<n;r++)
         {
-            dfs(heights, r,0,INT_MIN,pac);
+            dfs(heights,r,0,INT_MIN,pac);
             dfs(heights,r,m-1,INT_MIN,atl);
         }
         vector<int>v;
@@ -51,11 +103,9 @@ public:
                     v.push_back(i);
                     v.push_back(j);
                     ans.push_back(v);
-                    
                 }
             }
         }
-        
         return ans;
         
     }
