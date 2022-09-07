a594d810fec662fe8163177faec133307aba464f
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-09-08 01:09:55 +0530

    Time: 8 ms (68.85%), Space: 8.3 MB (41.55%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -10,71 +10,29 @@
  */
 class Solution {
 public:
-     ListNode* reverseList(ListNode* head) {
-//         if(head==NULL)
-//         {
-//             return NULL;
-//         }
-//       ListNode*prevptr=NULL;
-//       ListNode*currptr=head;
-//       ListNode*nextptr;
-//          while(currptr!=NULL)
-//          {
-//                 nextptr=currptr->next;
-//                 currptr->next=prevptr;
-//                 prevptr=currptr;
-//                 currptr=nextptr;
-             
-//          }
-//          return prevptr;
-         // ___________________________--
-//          if(head==NULL||head->next==NULL)
-//          {
-//                 return head;
-//          }
-         
-//          ListNode*newhead=reverseList(head->next);
-//          head->next->next=head;
-//          head->next=NULL;
-//          return newhead;
-         // _________________________---
-         
-//          ListNode*curr=head;
-//          ListNode*prev=NULL;
-//          ListNode*nexti;
-//          while(curr!=NULL)
-//          {
-//              nexti=curr->next;
-//              curr->next=prev;
-//              prev=curr;
-//              curr=nexti;
-//          }
-         
-//         return prev;
-         // _____________________-
-         // ListNode*curr=head;
-         // ListNode*prev=NULL;
-         // ListNode*nextptr;
-         // while(curr!=NULL)
-         // {
-         //     nextptr=curr->next;
-         //     curr->next=prev;
-         //     prev=curr;
-         //     curr=nextptr;
-         // }
-         // return prev;
-         // -------------
-         ListNode*curr=head;
-         ListNode*prev=NULL;
-         ListNode*nexi;
-         while(curr!=NULL)
-         {
-             nexi=curr->next;
-             curr->next=prev;
-             prev=curr;
-             curr=nexi;
-         }
-         return prev;
-         
+    ListNode* reverseList(ListNode* head) {
+        // ListNode*curr=head;
+        // ListNode*prev=NULL;
+        // ListNode*nexi;
+        // while(curr!=NULL)
+        // {
+        //     nexi=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=nexi;
+        // }
+        // return prev;
+        
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*nexi;
+        while(curr!=NULL)
+        {
+            nexi=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nexi;
+        }
+        return prev;
     }
 };
