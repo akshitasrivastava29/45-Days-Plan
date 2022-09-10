995349730a2c4f0b6429942809dc1805c42dc9c8
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-09-11 01:48:39 +0530

    Time: 62 ms (12.67%), Space: 15.1 MB (14.75%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,29 +1,65 @@
 class Solution {
-    void dfs(int src,vector<int>adjl[],vector<int>&vis)
+//     void dfs(int src,vector<int>adjl[],vector<int>&vis)
+//     {
+//         vis[src]=1;
+//         for(auto it:adjl[src])
+//         {
+//             if(vis[it]!=1)
+//             {
+//                 dfs(it,adjl,vis);
+//             }
+            
+//         }
+    // }
+    void dfs(int node,vector<int>adjL[],vector<int>&vis)
     {
-        vis[src]=1;
-        for(auto it:adjl[src])
+        vis[node]=1;
+        for(auto it:adjL[node])
         {
             if(vis[it]!=1)
             {
-                dfs(it,adjl,vis);
+                dfs(it,adjL,vis);
             }
-            
         }
     }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
+        // int n=isConnected.size();
+        // vector<int>adjl[n];
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(isConnected[i][j]==1 && i!=j)
+        //         {
+        //             adjl[i].push_back(j);
+        //             adjl[j].push_back(i);
+        //         }
+        //     }
+        // }
+        // vector<int>vis(n,0);
+        // int cnt=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(vis[i]==0)
+        //     {
+        //         dfs(i,adjl,vis);
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+        
         int n=isConnected.size();
-        vector<int>adjl[n];
+        vector<int>adjL[n];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(isConnected[i][j]==1 && i!=j)
                 {
-                    adjl[i].push_back(j);
-                    adjl[j].push_back(i);
-                }
+                    adjL[i].push_back(j);
+                    adjL[j].push_back(i);
+                }  
             }
         }
         vector<int>vis(n,0);
@@ -32,10 +68,11 @@ public:
         {
             if(vis[i]==0)
             {
-                dfs(i,adjl,vis);
+                dfs(i,adjL,vis);
                 cnt++;
             }
         }
         return cnt;
+        
     }
 };
