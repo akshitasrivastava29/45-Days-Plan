e6ff15e9e9b297e1b72e68aa25f50386ea19ef7e
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-09-14 22:15:02 +0530

    Time: 16 ms (39.76%), Space: 6.8 MB (59.86%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -3,31 +3,29 @@ public:
     string multiply(string num1, string num2) {
         int n=num1.size();
         int m=num2.size();
-        if(num1=="0" || num2=="0")
-        {
-            return "0";
-        }
-        vector<int>num(n+m,0);
+        if(num1=="0" || num2=="0") return "0";
+        vector<int>v(n+m,0);
         for(int i=n-1;i>=0;i--)
         {
             for(int j=m-1;j>=0;j--)
             {
-                num[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
-                num[i+j]+=num[i+j+1]/10;
-                num[i+j+1]%=10;
+                v[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+                v[i+j]+=v[i+j+1]/10;
+                v[i+j+1]%=10;
             }
         }
         int i=0;
-        while(i<num.size() && num[i]==0)
+        while(i<v.size() && v[i]==0)
         {
             i++;
         }
         string res="";
-        while(i<num.size())
+        while(i<v.size())
         {
-            res.push_back(num[i++]+'0');
+            res.push_back(v[i++]+'0');
+            
         }
-        return res;
+       return res; 
         
     }
 };
