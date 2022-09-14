fd4e09928d18b1c28859c9c1f1520b1f874eaf3f
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-09-14 19:45:13 +0530

    Time: 94 ms (5.19%), Space: 71.5 MB (51.80%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -37,6 +37,32 @@ public:
         // }
         //   return dummy->next;
         
+        // ListNode*dummy=new ListNode(-1);
+        // ListNode*p1=l1;
+        // ListNode*p2=l2;
+        // ListNode*p3=dummy;
+        // int carry=0;
+        // while(p1!=NULL || p2!=NULL || carry)
+        // {
+        //     int sum=0;
+        //     if(p1!=NULL)
+        //     {
+        //         sum+=p1->val;
+        //         p1=p1->next;
+        //     }
+        //     if(p2!=NULL)
+        //     {
+        //         sum+=p2->val;
+        //         p2=p2->next;
+        //     }
+        //     sum+=carry;
+        //     carry=sum/10;
+        //     ListNode*newnode=new ListNode(sum%10);
+        //     p3->next=newnode;
+        //     p3=p3->next;
+        // }
+        // return dummy->next;
+        
         ListNode*dummy=new ListNode(-1);
         ListNode*p1=l1;
         ListNode*p2=l2;
@@ -57,7 +83,7 @@ public:
             }
             sum+=carry;
             carry=sum/10;
-            ListNode*newnode=new ListNode(sum%10);
+            ListNode*newnode =new ListNode(sum%10);
             p3->next=newnode;
             p3=p3->next;
         }
