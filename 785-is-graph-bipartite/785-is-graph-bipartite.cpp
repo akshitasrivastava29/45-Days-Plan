a09f0d9f0ae4671b5fa7a4566bef150620e40e59
akshitasrivastava29 <93863456+akshitasrivastava29@users.noreply.github.com>
2022-09-19 22:22:47 +0530

    Time: 73 ms (9.65%), Space: 13.7 MB (41.99%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,87 +1,12 @@
 class Solution {
-    // private:
-    // bool bipartitebfs(int src,vector<vector<int>>&graph,int color[])
-    // {
-    //     queue<int>q;
-    //     q.push(src);
-    //     color[src]=1;
-    //     while(!q.empty())
-    //     {
-    //         int node=q.front();
-    //         q.pop();
-    //         for(auto it: graph[node])
-    //         {
-    //             if(color[it]==-1)
-    //             {
-    //                 color[it]=1-color[node];
-    //                 q.push(it);
-    //             }
-    //             else if(color[it]==color[node])
-    //             {
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    // private:
-    // bool checkbipartitiebfs(int src,vector<vector<int>>&graph,int color[])
-    // {
-    //     queue<int>q;
-    //     q.push(src);
-    //     color[src]=1;
-    //     while(!q.empty())
-    //     {
-    //         int node=q.front();
-    //          q.pop();
-    //         for(auto it:graph[node])
-    //         {
-    //             if(color[it]==-1)
-    //             {
-    //                 color[it]=1-color[node];
-    //                 q.push(it);
-    //             }
-    //             else if(color[it]==color[node])
-    //             {
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     return true;
-    // }
-    private:
-//     bool bipartitebfs(int st,vector<vector<int>>&graph,int color[])
-//     {
-//         queue<int>q;
-//         q.push(st);
-//         color[st]=1;
-//         while(!q.empty())
-//         {
-//             int node=q.front();
-//             q.pop();
-//             for(auto it:graph[node])
-//             {
-//                if(color[it]==-1)
-//                {
-//                 color[it]=1-color[node];
-//                 q.push(it);
-//                }
-//                 else if(color[it]==color[node])
-//                 {
-//                         return false;
-//                 }
-                    
-//             }
-//         }
-//         return true;
-//     }
-    // ________________________________--
-    bool checkbipartitiebfs(int src,vector<vector<int>>&graph,int color[])
+    bool checkBipartite(int src,vector<vector<int>>& graph,int color[])
     {
+        int n=graph.size();
         queue<int>q;
         q.push(src);
         color[src]=1;
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int node=q.front();
             q.pop();
             for(auto it:graph[node])
@@ -91,63 +16,15 @@ class Solution {
                     color[it]=1-color[node];
                     q.push(it);
                 }
-                else if(color[it]==color[node])
-                {
+                else if(color[it]==color[node]){
                     return false;
                 }
             }
-           
         }
-         return true;
+        return true;
     }
-    
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        // int n=graph.size();
-        // int color[n];
-        // memset(color,-1,sizeof color);
-        // for(int i=0;i<n;i++)
-        //     {
-        //         if(color[i]==-1)
-        //         {
-        //             if(!bipartitebfs(i,graph,color))
-        //             {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        // return true;
-        // ________________________---
-        // int n=graph.size();
-        // int color[n];
-        // memset(color,-1,sizeof color);
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(color[i]==-1)
-        //     {
-        //         if(!bipartitebfs(i,graph,color))
-        //         {
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
-        // __________________________________--
-        // int n=graph.size();
-        // int color[n];
-        // memset(color,-1,sizeof(color));
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(color[i]==-1)
-        //     {
-        //         if(!checkbipartitiebfs(i,graph,color))
-        //         {
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
-        // ---------------
         int n=graph.size();
         int color[n];
         memset(color,-1,sizeof(color));
@@ -155,12 +32,13 @@ public:
         {
             if(color[i]==-1)
             {
-                if(!checkbipartitiebfs(i,graph,color))
+                if(!checkBipartite(i,graph,color))
                 {
                     return false;
                 }
             }
         }
         return true;
+        
     }
 };
